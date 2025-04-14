599ebe61cacc0c5b3f72f2d66decc8ac5ee5f5a3
Savita444 <savita.hajare@sumagoinfotech.com>
2025-04-14 23:16:51 +0530

    changes

--- config/DocumentConstant.php
+++ config/DocumentConstant.php
@@ -6,7 +6,7 @@ return [
 
     'QR_ADD'	         => "/all_web_data/images/qr/",
     'QR_DELETE'	         => '/all_web_data/images/qr/',
-    'QR_VIEW'	         => env("FILE_VIEW").'/all_web_data/images/qr/',
+    'QR_VIEW'	         => env("QR_VIEW").'/all_web_data/images/qr/',
 
     'HOTEL_IMAGE_ADD'	         => "/all_web_data/images/hotel_image/",
     'HOTEL_IMAGE_DELETE'	         => '/all_web_data/images/hotel_image/',
